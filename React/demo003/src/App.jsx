cfa620e908d7153b39f58624d9678402794959b5
Gautam Mukherjee <mukherjeegautam100@gmail.com>
2025-11-26 22:02:55 +0530

    React

--- React/demo003/src/App.jsx
+++ React/demo003/src/App.jsx
@@ -1,7 +1,8 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import UseReff from "./components/useReff";
 import PassFunc from "./components/PassFunc";
 import UserData from "./components/UserData";
+import ForwardReff from "./components/ForwardReff";
 
 function App() {
   let [counter, setCounter] = useState(0);
@@ -21,11 +22,16 @@ function App() {
     callWithLoad2();
   },[counter1]);
 
-
-
   const handleClick = () => {
     setCounter(counter + 1);
   };
+
+  const inputRef = useRef(null)
+  const updateInput = () =>{
+    inputRef.current.value = inputRef.current.value.toUpperCase()
+    inputRef.current.style.color = "red"
+    inputRef.current.focus()
+  }
   return (
     <>
       <h1>useEffect</h1>
@@ -37,6 +43,12 @@ function App() {
       <PassFunc />
       <hr />
       <UserData />
+      <h1>Forward Ref</h1>
+
+      <button onClick={()=>updateInput()}>Click here</button>
+      <ForwardReff ref={inputRef} />
+
+
     </>
   );
 }
--- React/demo003/src/components/UserData.jsx
+++ React/demo003/src/components/UserData.jsx
@@ -5,48 +5,62 @@ const UserData = () => {
   const [email, setEmail] = useState("");
   const [mobile, setMobile] = useState("");
 
-  const [userData,setUserData] = useState([])
-  const handleAdd = () =>{
+  const [userData, setUserData] = useState([]);
+  const handleAdd = () => {
     let obj = {
-        "name":name,
-        "email": email,
-        "mobile":mobile
-    }
-    setUserData(userData.push(obj))
-    console.log(userData)
-  }
+      name: name,
+      email: email,
+      mobile: mobile,
+    };
+    //setUserData(userData.push(obj))
+    setUserData([...userData, obj]);
+    console.log(userData);
+  };
   return (
     <div>
-        <input type="text" id="user_name" placeholder="Enter Name" onChange={(e)=>setName(e.target.value)} />
-        <br />
-        <br />
-        <input type="email" id="user_email" placeholder="Enter Email" onChange={(e)=>setEmail(e.target.value)} />
-        <br />
-        <br />
-        <input type="text" id="user_mobile" placeholder="Enter Mobile" onChange={(e)=>setMobile(e.target.value)} />
-        <br />
-        <br />
-        <button onClick={()=>handleAdd()}>Add</button>
-        <table>
-            <thead>
-                <tr>
-                    <th>Name</th>
-                    <th>Email</th>
-                    <th>Mobile</th>
-                </tr>
-            </thead>
-            <tbody>
-                {
-                 userData.map((user)=>{
-                    <tr>
-                        <td>{user.name}</td>
-                        <td>{user.email}</td>
-                        <td>{user.mobile}</td>
-                    </tr>
-                 })   
-                }
-            </tbody>
-        </table>
+      <input
+        type="text"
+        id="user_name"
+        placeholder="Enter Name"
+        onChange={(e) => setName(e.target.value)}
+      />
+      <br />
+      <br />
+      <input
+        type="email"
+        id="user_email"
+        placeholder="Enter Email"
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <br />
+      <br />
+      <input
+        type="text"
+        id="user_mobile"
+        placeholder="Enter Mobile"
+        onChange={(e) => setMobile(e.target.value)}
+      />
+      <br />
+      <br />
+      <button onClick={() => handleAdd()}>Add</button>
+      <table border="1">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Mobile</th>
+          </tr>
+        </thead>
+        <tbody>
+          {userData.map((user) => (
+            <tr key={user.email}>
+              <td>{user.name}</td>
+              <td>{user.email}</td>
+              <td>{user.mobile}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 };
